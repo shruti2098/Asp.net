dc1a1bf0954818bbde4bdcd39ccd4377d2b17584
shruti2098 <shrutiiadam2098@gmail.com>
2022-03-10 16:31:10 +0530

    files added

---
+++ LinqProject/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using LinqProject.DemoLinq;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Stud()
+        {
+            TestStudentRepository repo = new TestStudentRepository();
+            List<Student> details = repo.Stud();
+            return Ok(details);
+        }
+       
+        [HttpGet]
+        public ActionResult GetStudentById(int id)
+        {
+            TestStudentRepository repo = new TestStudentRepository();
+            Student details = repo.GetStudentById(id);
+            return Ok(details);
+        }
+
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ LinqProject/DemoLinq/TestStudentRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqProject.DemoLinq
+{
+    public class TestStudentRepository:IStudentRepository
+    {
+        public List<Student> DataSource()
+        {
+
+            return new List<Student>()
+            {
+                new Student()
+                {
+                    id=1,
+                    name="Aishwarya",
+                    address="Soalpur"
+                },
+               new Student()
+               {
+                id = 2,
+                name = "Arpita",
+                address = "Pune"
+               },
+            new Student()
+               {
+                id = 3,
+                name = "Varsha",
+                address = "Banglore"
+               },
+        };
+            
+        }
+        public Student GetStudentById(int id)
+        {
+            return DataSource().FirstOrDefault(st => st.id == id);
+            //return DataSource().First(st => st.id == id);
+            // return DataSource().SingleOrDefault(st => st.id == id);
+            //return DataSource().Single(st => st.id == id);
+
+
+        }
+        public List<Student> Stud()
+        {
+            return DataSource();
+        }
+
+    }
+}
