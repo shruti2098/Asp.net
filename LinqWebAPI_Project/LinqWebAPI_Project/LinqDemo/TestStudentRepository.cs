2fca63980b533cdcefe709dfdc4c0c29e0f63d1a
shruti2098 <shrutiiadam2098@gmail.com>
2022-03-16 17:34:59 +0530

    files added

---
+++ LinqWebAPI_Project/LinqWebAPI_Project/LinqDemo/TestStudentRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqWebAPI_Project.LinqDemo
+{
+    public class TestStudentRepository : IStudentRepository
+    {
+        public List<Student> DataSource()
+        {
+
+            return new List<Student>()
+            {
+                new Student()
+                {
+                    id=1,
+                    name="Shruti",
+                    address="Ahmednagar"
+                },
+               new Student()
+               {
+                id = 2,
+                name = "Rohini",
+                address = "Pune"
+               },
+            new Student()
+               {
+                id = 3,
+                name = "Trisha",
+                address = "Mumbai"
+               },
+            new Student()
+            {
+                id=4,
+                name="Rahul",
+                address="Nasik"
+            },
+        };
+
+        }
+        public Student GetStudentById(int id)
+        {
+            return DataSource().FirstOrDefault(st => st.id == id);
+
+        }
+        public List<Student> Stud()
+        {
+            return DataSource();
+        }
+
+    }
+}
---
+++ LinqWebAPI_Project/LinqWebAPI_Project/Services/StudentServices.cs
@@ -0,0 +1,29 @@
+﻿using LinqWebAPI_Project.LinqDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqWebAPI_Project.Services
+{
+    public class StudentServices
+    {
+        private readonly IStudentRepository _repo = null;
+
+        public StudentServices(IStudentRepository repo)
+        {
+            _repo = repo;
+        }
+        public List<Student> Stud()
+        {
+            return _repo.Stud();
+        }
+
+        public Student GetStudentById(int StudentId)
+        {
+            return _repo.GetStudentById(StudentId);
+        }
+    }
+
+}
+
